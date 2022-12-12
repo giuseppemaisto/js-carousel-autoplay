c7c4a7b523147cce40b7783598170469333d9add
giuseppemaisto <gmaisto98@gmail.com>
2022-12-12 15:41:17 +0100

    autoplay inserito

--- js/script.js
+++ js/script.js
@@ -97,3 +97,35 @@ prev.addEventListener('click', function(){
 })
 
 
+//1- definisco costante autoplay
+//  1.1 - definosco costante come SetInterval
+// 1.2 - do come parametri a setinterval funzione che mi farà scorrere le immagini 
+
+let autoPlay;
+autoPlay = setInterval(auto, 2000);
+//2.- definisco funzione che fa scorrere le immagini( uso codice già presente da vecchio esercizio )
+function auto(){
+    //verifico l'elemento attivo (itemActive)
+ 
+  //incremento il suo valore di 1
+ itemActive++;
+ if(itemActive == imagesArray.length){
+  itemActive = 0;
+ }
+//aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
+  //stessa cosa per i cerchi
+ if(itemActive == 0){
+  items[imagesArray.length-1].classList.remove('active');
+  circles[imagesArray.length-1].classList.remove('active')
+ }
+ else{
+  items[itemActive-1].classList.remove('active');
+  circles[itemActive-1].classList.remove('active');
+ }
+ items[itemActive].classList.add('active')
+ circles[itemActive].classList.add('active');
+ 
+ 
+
+
+}
